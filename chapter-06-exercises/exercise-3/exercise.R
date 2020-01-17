b519da22af839527ecd449f73563ebda1b4db76d
Aaron Ng <AaronNg@Aarons-MacBook.local>
2020-01-16 16:09:11 -0800

    Add Week 2 Exercises

--- chapter-06-exercises/exercise-3/exercise.R
+++ chapter-06-exercises/exercise-3/exercise.R
@@ -3,9 +3,17 @@
 # Define a function `add_three` that takes a single argument and
 # returns a value 3 greater than the input
 
+add_three <- function(number)
+{
+  return( number + 3)
+}
+
 
 # Create a variable `ten` that is the result of passing 7 to your `add_three` 
 # function
+
+ten <- add_three(7)
+print(ten)
 
 
 # Define a function `imperial_to_metric` that takes in two arguments: a number 
--- chapter-07-exercises/exercise-1/exercise.R
+++ chapter-07-exercises/exercise-1/exercise.R
@@ -2,31 +2,40 @@
 
 # Create a vector `names` that contains your name and the names of 2 people 
 # next to you. Print the vector.
-
+vec <- c("Natalie", "Aaron")
+print(vec)
 
 # Use the colon operator : to create a vector `n` of numbers from 10:49
-
+n <- 10:49
+print(n)
 
 # Use the `length()` function to get the number of elements in `n`
-
+length(n)
 
 # Add 1 to each element in `n` and print the result
-
+v2 <- c(1)
+v3 <- n + v2
 
 # Create a vector `m` that contains the numbers 10 to 1 (in that order). 
 # Hint: use the `seq()` function
+?seq
 
+m <- seq(10, 1)
 
 # Subtract `m` FROM `n`. Note the recycling!
-
+v4 <- n - m
 
 # Use the `seq()` function to produce a range of numbers from -5 to 10 in `0.1`
 # increments. Store it in a variable `x_range`
 
+x_range <- seq(-5, 10, 0.1)
 
 # Create a vector `sin_wave` by calling the `sin()` function on each element 
 # in `x_range`.
 
+?sin
+
+sin_wave <- sin(x_range)
 
 # Create a vector `cos_wave` by calling the `cos()` function on each element 
 # in `x_range`.
@@ -35,6 +44,10 @@
 # Create a vector `wave` by multiplying `sin_wave` and `cos_wave` together, then
 # adding `sin_wave` to the product
 
+cos_wave <- cos(x_range)
+wave <- (sin_wave * cos_wave) + sin_wave  
 
 # Use the `plot()` function to plot your `wave`!
 
+?plot
+plot(wave)
